603a1cf4ab0f978187c3ce9b24e2dd5d82fc5c6d
your github.com username <kandeel.a1@gmail.com>
2017-02-27 16:01:07 -0500

    add routes, index, show, edit, new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get "users/index"
+get "users/show"
+get "users/edit"
+get "users/new"
 
 
 end
