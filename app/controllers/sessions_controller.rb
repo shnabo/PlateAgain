1daf0796aa04d48d098e0292138a084970bf4112
Sathes Balasingam <sathes352@gmail.com>
2017-02-27 19:21:37 -0500

    fixed sessions controller, fixed redirect in users controlle, created new users form, started dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
   end
+
   def create
-    @user = User.new
-   if @user.save
-     redirect_to root_url
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to show_url, notice: "Logged In!"
    else
      render :new
    end
@@ -14,4 +15,6 @@ end
 
 def user_params
   params.require(:user).permit(:email, :password, :password_confirmation)
-add sessions model, form
+
+end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
       if @user.save
-        redirect_to user_show_url
+        redirect_to users_url
       else
         render :new
       end
