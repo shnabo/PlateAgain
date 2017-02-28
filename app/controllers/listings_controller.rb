b042597ef91bef480669d5b8e17e4093d726c515
sylvia villegas <sylvia@svillegas.com>
2017-02-28 16:20:46 -0500

    Complete REST actions, include user

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,18 +1,67 @@
 class ListingsController < ApplicationController
 
+  before_action :load_user
+
+  def all
+    @listings = Listing.all
+  end
+
+  def index
+    @listings = @user.listings
+  end
+
+  def show
+    find_listing
+  end
 
   def new
     @listing = Listing.new
   end
-  def index
-    @listings = Listing.all
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.user = @user
+
+    if @listing.save
+      redirect_to dashboard_path(@user)
+    else
+      render :new
+    end
   end
 
-  def show
+  def edit
+    find_listing
+  end
+
+  def update
+    find_listing
+
+    if @listing.update(listing_params)
+      redirect_to dashboard_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    find_listing
+    @listing.destroy
+    redirect_to dashboard_path(@user)
+  end
+
+
+  private
+
+  def load_user
+    @user = User.find(params[:user_id])
+  end
+
+  def find_listing
     @listing = Listing.find(params[:id])
   end
 
-  def mylistings
-    @listings = Listing.find(params[:user_id])
+  def listing_params
+    params.require(:listing).permit(:item_name, :quantity, :perishable, :expiry_date, :available_until, :contact_name, :contact_phone, :contact_email, :picture, :notes, :user_id)
   end
+
 end
