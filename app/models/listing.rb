5d721990e7c9a6981a12bb8a29dcc7b78402bd0d
Sathes Balasingam <sathes352@gmail.com>
2017-03-01 21:11:38 -0500

    Tried to finish validating Dates, almost got it done

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,21 +2,19 @@ class Listing < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :item_name, :quantity, :perishable
-  def does_not_start_in_past
-    if start_date.present? && start_date < Date.today
-      errors.add(:start_date, "must be today's date")
+
+  validate :issue_expiry_date, :available_until_issue
+
+  def issue_expiry_date
+    if expiry_date.present? && expiry_date < Date.today
+      errors.add(:expiry_date, "must be at least today's date")
     end
   end
 
-  def does_not_start_in_future
-    if start_date.present? && start_date > Date.today
-      errors.add(:start_date, "must be today's date")
+  def available_until_issue
+    if available_until.present? && available_until < Date.today
+      errors.add(:available_until, "must be later then now")
     end
   end
 
-  def expiration_date_is_not_in_past
-    if end_date.present? && end_date > Date.today
-      errors.add(:end_date, "must be in the future")
-    end
-  end
 end
