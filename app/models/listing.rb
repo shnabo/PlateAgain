2675b554673b4a31b62cf38d54cf94816d93485c
your github.com username <kandeel.a1@gmail.com>
2017-03-02 15:28:04 -0500

    fixed bug in listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,22 +1,25 @@
 class Listing < ApplicationRecord
   belongs_to :user
 
+  validate :expiry_date
+  validate :available_until
   validates_presence_of :item_name, :quantity, :perishable
-  def does_not_start_in_past
+
+  def expiry_date_not_past
     if start_date.present? && start_date < Date.today
-      errors.add(:start_date, "must be today's date")
+      errors.add(:expiry_date, "must be today's date")
     end
   end
 
-  def does_not_start_in_future
-    if start_date.present? && start_date > Date.today
-      errors.add(:start_date, "must be today's date")
+  def available_until_not_past
+    if expiry_date.present? && start_date < Date.today
+      errors.add(:expiry_date, "must be today's date")
     end
   end
 
   def expiration_date_is_not_in_past
-    if end_date.present? && end_date > Date.today
-      errors.add(:end_date, "must be in the future")
+    if available_until.present? && end_date < Date.today
+      errors.add(:available_until, "must be in the future")
     end
   end
 end
