5d3317b013165770823a15075bb602510bcc48e0
Steve Villella <s.villella@live.com>
2017-03-02 14:10:51 -0500

    Fix Listing Model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,7 +6,7 @@ class Listing < ApplicationRecord
   # Enable image upload with Carrierwave
   mount_uploader :photo, ImageUploader
 
-  validate :issue_expiry_date, :available_until_issue, :expiry_date_not_larger_than_available
+  validate :issue_expiry_date, :available_until_issue
 
   def issue_expiry_date
     if expiry_date.present? && expiry_date < Date.today
@@ -20,10 +20,6 @@ class Listing < ApplicationRecord
     end
   end
 
-  def expiry_date_not_larger_than_available
-    if available_until < expiry_date
-      errors.add(:expiry_date, "must be earlier than available date")
-    end
-  end
+
 
 end
