f20a8cc65a100155cad1b267c97cf63be9229f95
Sathes Balasingam <sathes352@gmail.com>
2017-03-02 11:06:45 -0500

    Expiry date must be earlier than available date

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,7 +6,7 @@ class Listing < ApplicationRecord
   # Enable image upload with Carrierwave
   mount_uploader :photo, ImageUploader
 
-  validate :issue_expiry_date, :available_until_issue
+  validate :issue_expiry_date, :available_until_issue, :expiry_date_not_larger_than_available
 
   def issue_expiry_date
     if expiry_date.present? && expiry_date < Date.today
@@ -17,6 +17,12 @@ class Listing < ApplicationRecord
   def available_until_issue
     if available_until.present? && available_until < Date.today
       errors.add(:available_until, "must be later then now")
+    end
+  end
+
+  def expiry_date_not_larger_than_available
+    if available_until < expiry_date
+      errors.add(:expiry_date, "must be earlier than available date")
     end
   end
 
