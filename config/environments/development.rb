9476014235d18c8b1eb2711819326aa5382aa79a
Sathes Balasingam <sathes352@gmail.com>
2017-03-04 16:13:41 -0500

    Added information to development.rb in order to send email, I'm assuming it's not working because I don't have a domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  #Change mail delivery
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "plateagaintoronto@gmail.com",
+    port: 587,
+    authentication: "plain",
+    user_name: ENV[""],
+    password: ENV[""]
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
