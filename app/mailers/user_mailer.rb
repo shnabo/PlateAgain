3b5c3e4c3ba274268ba915f718b35ec2305f962d
Sathes Balasingam <sathes352@gmail.com>
2017-03-04 13:14:33 -0500

    Added From, To and Subject in user_mailer, and sign up message to signup_confirmation.text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,13 @@
 class UserMailer < ApplicationMailer
-
+  default from: "PlateAgain@plateagain.com"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.signup_confirmation.subject
   #
-  def signup_confirmation
-    @greeting = "Hi"
+  def signup_confirmation(user)
+    @user = user
 
-    mail to: "to@example.org"
+    mail to: "user.email", subject: "Sign Up Confirmation"
   end
 end
