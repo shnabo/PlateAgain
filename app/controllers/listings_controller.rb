efb2b90c4b0044d2ae38a97fab58082ab549a824
your github.com username <kandeel.a1@gmail.com>
2017-03-04 19:24:47 -0500

    change code from index page to listings controller. it should not be in view.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -30,7 +30,6 @@ class ListingsController < ApplicationController
 
   def update
     find_listing
-
     if @listing.update(listing_params)
       redirect_to dashboard_path(current_user.id)
     else
@@ -56,7 +55,21 @@ class ListingsController < ApplicationController
   end
 
   def listing_params
-    params.require(:listing).permit(:item_name, :quantity, :perishable, :expiry_date, :available_until, :contact_name, :contact_phone, :contact_email, :picture, :notes, :user_id)
+    params.require(:listing).
+    permit(
+    :item_name,
+    :quantity,
+    :perishable,
+    :expiry_date,
+    :available_until,
+    :contact_name,
+    :contact_phone,
+    :contact_email,
+    :picture,
+    :notes,
+    :user_id
+    )
+
   end
 
 end
