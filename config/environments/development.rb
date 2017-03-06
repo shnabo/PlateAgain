859c1659a4042fa0d2fcfdd5c83020026b3950e8
Sathes Balasingam <sathes352@gmail.com>
2017-03-06 14:24:32 -0500

    Commented out stuff in development.rb to get the errors fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,22 +27,22 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
   #Change mail delivery
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "plateagaintoronto@gmail.com",
-    port: 587,
-    authentication: "plain",
-    user_name: ["plateagaintoronto@gmail.com"],
-    password: ["bitmakersasps"],
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: "plateagaintoronto@gmail.com",
+  #   port: 587,
+  #   authentication: "plain",
+  #   user_name: ["plateagaintoronto@gmail.com"],
+  #   password: ["bitmakersasps"],
+  #   :enable_starttls_auto => true
+  # }
 
-  config.action_mailer.default_url_options = { host: "localhost:3000"}
+  # config.action_mailer.default_url_options = { host: "localhost:3000"}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
