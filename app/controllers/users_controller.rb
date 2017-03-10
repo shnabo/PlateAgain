2ceab8f07d24b5c7add1782b4252704aefd51547
sylvia villegas <sylvia@svillegas.com>
2017-03-10 15:06:32 -0500

    Show listings on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,12 @@ class UsersController < ApplicationController
 
   def listings
     load_user
-    @listings = @user.listings
+    load_user_listings
   end
 
  def show
-   @user = load_user
+   load_user
+   load_user_listings
    if current_user
      @current_address = [current_user.add_number,
                          current_user.add_name,
@@ -57,11 +58,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = load_user
+    load_user
   end
 
   def update
-    @user = load_user
+    load_user
 
     if @user.update_attributes(user_params)
       redirect_to dashboard_path, notice: "Profile updated!"
@@ -73,7 +74,7 @@ class UsersController < ApplicationController
 
   def dashboard
     @user = current_user
-    @listings = @user.listings
+    load_user_listings
   end
 
 
@@ -81,6 +82,10 @@ class UsersController < ApplicationController
 
   def load_user
     @user = User.find(params[:id])
+  end
+
+  def load_user_listings
+    @listings = @user.listings
   end
 
   def user_params
