1856efa66ece060acd95a0082343c34d5b2f4adc
sylvia villegas <sylvia@svillegas.com>
2017-03-10 10:27:24 -0500

    Add relationship to reviewer in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-belongs_to :user
-
+  belongs_to :user, class_name: "User", foreign_key: :user_id
+  belongs_to :reviewer, class_name: "User", foreign_key: :reviewer_id
 end
