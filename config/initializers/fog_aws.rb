0aa3b2cb14abaf330406abd8119873d7e394bfff
sylvia villegas <sylvia@svillegas.com>
2017-03-17 19:18:50 -0400

    Try new styles

--- config/initializers/fog_aws.rb
+++ config/initializers/fog_aws.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],          # required
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],      # required
+    aws_access_key_id:     Rails.application.secrets.AWS_ACCESS_KEY_ID,          # required
+    aws_secret_access_key: Rails.application.secrets.AWS_SECRET_ACCESS_KEY,
     region:                'ca-central-1',                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
