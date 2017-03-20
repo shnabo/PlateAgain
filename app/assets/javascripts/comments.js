ffb67fa45a706e343db27b1ff56451fb60326ebc
sylvia villegas <sylvia@svillegas.com>
2017-03-20 12:49:36 -0400

    Wrap script in document ready

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,16 +1,25 @@
-var reviewSubmit = $('#new_comment > input[type="submit"]');
-var commentInput = $('#comment_comments');
+$(document).ready(function() {
+  var reviewSubmit = $('#new_comment > input[type="submit"]');
+  var commentInput = $('#comment_comments');
 
-function disableSubmitReview() {
-  reviewSubmit.attr('disabled', 'disabled');
-}
-
-disableSubmitReview();
-
-commentInput.on('keyup', function() {
-  if (commentInput.val() === "") {
-    disableSubmitReview();
-  } else {
-    reviewSubmit.removeAttr('disabled');
+  function disableSubmitReview() {
+    reviewSubmit.attr('disabled', 'disabled');
   }
+
+  disableSubmitReview();
+
+  commentInput.on('keyup', function() {
+    if (commentInput.val() === "") {
+      disableSubmitReview();
+    } else {
+      reviewSubmit.removeAttr('disabled');
+    }
+  });
+
 });
+
+
+// reviewSubmit.on('submit', function(event) {
+//   event.preventDefault();
+//   $('.comment-list').append();
+// });
