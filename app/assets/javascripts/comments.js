c6c2e0c8db27ce544512926297599a93473e7eb2
sylvia villegas <sylvia@svillegas.com>
2017-03-20 11:57:57 -0400

    Wrote .js to disable empty review submission

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,16 @@
+var reviewSubmit = $('#new_comment > input[type="submit"]');
+var commentInput = $('#comment_comments');
+
+function disableSubmitReview() {
+  reviewSubmit.attr('disabled', 'disabled');
+}
+
+disableSubmitReview();
+
+commentInput.on('keyup', function() {
+  if (commentInput.val() === "") {
+    disableSubmitReview();
+  } else {
+    reviewSubmit.removeAttr('disabled');
+  }
+});
