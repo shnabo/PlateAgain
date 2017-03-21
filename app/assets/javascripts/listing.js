58e07ce9aa2fc22271c7ca2884f785073de1c6e3
sylvia villegas <sylvia@svillegas.com>
2017-03-21 00:51:25 -0400

    Just toggle '.card-claimed' when claim/unclaim button clicked

--- app/assets/javascripts/listing.js
+++ app/assets/javascripts/listing.js
@@ -1,25 +1,7 @@
-
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
-
 $(function () {
-
-  $('.listing input[type="submit"]').on('click', function() {
-          // event.preventDefault()
-          // $(this).toggleClass('green');
-    $('.listing').toggleClass('card-claimed');
-
-
+  
+  $('.listing input[type=submit]').on('click', function() {
+    $(this).parent().parent().toggleClass('card-claimed');
   });
-
-
-
-  // $('.claimed').on('click', function(){
-  //   $(this).text('.claim')
-  //   });
-  // });
-
-
 
 });
